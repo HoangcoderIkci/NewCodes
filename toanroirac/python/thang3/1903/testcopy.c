322fdc57dd40f50aaa7066f93d003aad5c762b60
HoangcoderIkci <phammanhhoang17012001@gmail.com>
2024-04-08 17:25:55 +0300

    08041725

--- toanroirac/python/thang3/1903/testcopy.c
+++ toanroirac/python/thang3/1903/testcopy.c
@@ -11,10 +11,10 @@
 #define I32 int32_t
 #define I16 int16_t
 #define I8 int8_t
-#define loop(i, a, b) for (U64 i = a; i < b; i++)
-#define glo_n 32
+#define loop(i, a, b) for (I64 i = a; i < b; i++)
+#define glo_n 30
 // const U32 glo_length = 1 << glo_n;
-#define glo_length 4294967296ULL // 16 65536ULL // 20 - 1048576ULL // 24 16777216ULL//28 268435456ULL // 30 1073741824ULL  //32 4294967296ULL //34 17179869184ULL
+#define glo_length 1073741824ULL // 16 65536ULL // 20 - 1048576ULL // 24 16777216ULL//28 268435456ULL // 30 1073741824ULL  //32 4294967296ULL //34 17179869184ULL
 
 void fastFindCoefficientsZhegalkin(U8 *lst_coefficients)
 {
@@ -88,39 +88,39 @@ void fastFindCoefficientsFourier(I8 *lst_coefficients)
         }
     }
 }
-U8 vesFunction(U64 f)
-{
-    U8 count = 0;
-    while (f)
-    {
-        count += f & 0b1;
-        f >>= 1;
-    }
-    return count;
-}
-U8 *calcF1()
-{
-    U8 *table = (U8 *)malloc(sizeof(U8) * glo_length);
-    U8 vec = 0;
-    U8 hafl_length = glo_n >> 1;
-    loop(i, 0, glo_length)
-    {
-        vec = vesFunction(i);
-        table[i] = (vec >= hafl_length);
-    }
-    return table;
-}
+// U8 vesFunction(U64 f)
+// {
+//     U8 count = 0;
+//     while (f)
+//     {
+//         count += f & 0b1;
+//         f >>= 1;
+//     }
+//     return count;
+// }
+// U8 *calcF1()
+// {
+//     U8 *table = (U8 *)malloc(sizeof(U8) * glo_length);
+//     U8 vec = 0;
+//     U8 hafl_length = glo_n >> 1;
+//     loop(i, 0, glo_length)
+//     {
+//         vec = vesFunction(i);
+//         table[i] = (vec >= hafl_length);
+//     }
+//     return table;
+// }
 
-U8 *calcF2()
+I8 *calcF2()
 {
-    U8 *table = (U8 *)malloc(sizeof(U8) * glo_length);
+    I8 *table = (I8 *)malloc(sizeof(I8) * glo_length);
     table[0] = 1;
     table[1] = 0;
     U64 current_leng = 2;
-    U8 *part2_table;
+    I8 *part2_table;
     while (current_leng < glo_length)
     {
-        memcpy(table + current_leng, table, current_leng * sizeof(U8));
+        memcpy(table + current_leng, table, current_leng * sizeof(I8));
         part2_table = table + current_leng;
         loop(i, 0, current_leng)
             part2_table[i] ^= 1;
@@ -131,20 +131,20 @@ U8 *calcF2()
 }
 
 // U16 c = 0;
-U64 f = 0;
+I64 f = 0;
 // U8 *global_table = (U8 *)calloc(glo_length, sizeof(U8));
 // Gán giá trị 1 cho tất cả các phần tử trong mảng bằng hàm memset
-U8 *global_table;
+I8 *global_table;
 void create_dynamic_array()
 {
-    global_table = (U8 *)malloc(glo_length * sizeof(U8)); // Cấp phát bộ nhớ cho mảng
-    memset(global_table, 1, glo_length * sizeof(U8));
+    global_table = (I8 *)malloc(glo_length * sizeof(I8)); // Cấp phát bộ nhớ cho mảng
+    memset(global_table, 1, glo_length * sizeof(I8));
 }
-void recursive(U64 t1, U16 num_loop)
+void recursive(I64 t1, I16 num_loop)
 {
     if (num_loop)
     {
-        U64 t2 = 1 << num_loop;
+        I64 t2 = 1 << num_loop;
         while (t2 < t1)
         {
             f ^= t2;
@@ -155,7 +155,7 @@ void recursive(U64 t1, U16 num_loop)
     }
     else
     {
-        U64 t2 = 1;
+        I64 t2 = 1;
         while (t2 < t1)
         {
             f ^= t2;
@@ -174,18 +174,18 @@ void calcF1SpAuto()
         recursive(glo_length, t);
     }
 }
-void checkF1()
-{
-    U8 *table1 = calcF1();
-    calcF1SpAuto();
-    // U8 *table2 = calcF1Supper();
-    loop(i, 0, glo_length) if (table1[i] != global_table[i])
-    {
-        printf("failed %u %u %u\n", i, table1[i], global_table[i]);
-    }
-    free(table1);
-    // free(table2);
-}
+// void checkF1()
+// {
+//     U8 *table1 = calcF1();
+//     calcF1SpAuto();
+//     // U8 *table2 = calcF1Supper();
+//     loop(i, 0, glo_length) if (table1[i] != global_table[i])
+//     {
+//         printf("failed %u %u %u\n", i, table1[i], global_table[i]);
+//     }
+//     free(table1);
+//     // free(table2);
+// }
 U8 *copyArray(U8 *arr, U32 size)
 {
     U8 *arrCp = (U8 *)malloc(sizeof(U8) * size);
@@ -228,7 +228,7 @@ int main()
 
     // giai đoạn 2
     start = clock();
-    fastFindCoefficientsZhegalkin(global_table);
+    fastFindCoefficientsReal(global_table);
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Thời gian thực thi giai đoạn 2: %f giây\n", cpu_time_used);
